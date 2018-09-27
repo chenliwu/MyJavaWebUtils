42d5df15c2b6dbe9230240d47cce4bf7dcde5b91
chenlw <>
2018-09-27 17:43:26 +0800

    发送邮件带附件

--- src/test/email/EmailSenderTest.java
+++ src/test/email/EmailSenderTest.java
@@ -1,10 +1,8 @@
 package email;
 
-import com.charlie.ssm.demo.email.RootConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
