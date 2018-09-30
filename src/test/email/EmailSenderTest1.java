889651328469b07e5b15cb3d3cf068b6d71b1515
chenlw <>
2018-09-30 10:13:18 +0800

    测试发送邮件的异常

--- src/test/email/EmailSenderTest1.java
+++ src/test/email/EmailSenderTest1.java
@@ -25,26 +25,29 @@ public class EmailSenderTest1 {
 
     @Test
     public void sendSimpleEmail() {
+        try{
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            Properties properties = new Properties();
+            properties.setProperty("mail.smtp.auth", "true");
+            properties.setProperty("mail.smtp.starttls.enable", "true");
+            //使用SSL，这个设置很关键
+            properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            mailSender.setJavaMailProperties(properties);
+            mailSender.setHost("smtp.exmail.qqzz.com");
+            mailSender.setPort(465);
+            mailSender.setUsername("123123@bytter.com");
+            mailSender.setPassword("123123");
 
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        Properties properties = new Properties();
-        properties.setProperty("mail.smtp.auth", "true");
-        properties.setProperty("mail.smtp.starttls.enable", "true");
-        //使用SSL，这个设置很关键
-        properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        mailSender.setJavaMailProperties(properties);
-        mailSender.setHost("smtp.exmail.qq.com");
-        mailSender.setPort(465);
-        mailSender.setUsername("123123@bytter.com");
-        mailSender.setPassword("123123");
-
-        SimpleMailMessage message = new SimpleMailMessage();//消息构造器
-        message.setFrom("123132@bytter.com");//发件人
-        message.setTo("1231231@qq.com");//收件人
-        message.setSubject("Spring Email Test");//主题
-        message.setText("hello world!!");//正文
-        mailSender.send(message);
-        System.out.println("邮件发送完毕");
+            SimpleMailMessage message = new SimpleMailMessage();//消息构造器
+            message.setFrom("12123123@bytter.com");//发件人
+            message.setTo("123131321@qq.com");//收件人
+            message.setSubject("Spring Email Test");//主题
+            message.setText("hello world!!");//正文
+            mailSender.send(message);
+            System.out.println("邮件发送完毕");
+        }catch (Exception e){
+            System.out.println("异常："+e.getMessage());
+        }
     }
 
     /**
@@ -65,13 +68,13 @@ public class EmailSenderTest1 {
         mailSender.setHost("smtp.exmail.qq.com");
         mailSender.setPort(465);
         mailSender.setUsername("12312321@bytter.com");
-        mailSender.setPassword("123123");
+        mailSender.setPassword("1231231");
 
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom("12313@bytter.com");
-            helper.setTo("1231232@qq.com");
+            helper.setFrom("12312@bytter.com");
+            helper.setTo("12312321@qq.com");
             helper.setSubject("Spring Email Test");
             helper.setText("<html><body><img src='cid:testLogo'>"
                     + "<h4>Hello World!!!</h4>"
@@ -79,11 +82,8 @@ public class EmailSenderTest1 {
 
             mailSender.send(message);
             System.out.println("邮件发送完毕");
-        } catch (MailAuthenticationException e1) {
-            System.out.println("message:"+e1.getMessage());
-            System.out.println("LocalizedMessage:"+e1.getLocalizedMessage());
-        } catch (MessagingException e2) {
-            System.out.println(e2.getMessage());
+        } catch (Exception e){
+            System.out.println("异常："+e.getMessage());
         }
 
     }
