8a01d5f9c280f309d14eef087ab6901d7209b667
chenlw <>
2018-10-24 17:31:03 +0800

    下载文件方式测试

--- src/main/java/com/charlie/ssm/demo/controller/TestParamController.java
+++ src/main/java/com/charlie/ssm/demo/controller/TestParamController.java
@@ -1,8 +1,11 @@
 package com.charlie.ssm.demo.controller;
 
+import com.charlie.ssm.demo.utils.NetworkUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/param")
@@ -11,6 +14,24 @@ public class TestParamController {
     @GetMapping("/page")
     public String page(){
         return "page/testParam";
+    }
+
+
+    @GetMapping("/testRequestIp")
+    @ResponseBody
+    public String testRequestIp(HttpServletRequest request){
+        StringBuilder sb = new StringBuilder();
+//        sb.append("getRemoteHost:"+request.getRemoteHost());  //发出请求的客户机的主机名
+//        sb.append("\n");
+//        sb.append("getLocalAddr:"+request.getLocalAddr());  //发出请求的IP地址
+//        System.out.println(sb.toString());
+        try {
+            sb.append(NetworkUtils.getIpAddress(request));
+            System.out.println(sb.toString());
+        }catch (Exception e){
+            System.out.println("异常："+e.getMessage());
+        }
+        return sb.toString();
     }
 
     /**
--- src/main/webapp/page/testDownFile.jsp
+++ src/main/webapp/page/testDownFile.jsp
@@ -31,7 +31,24 @@
 <script>
     $(function () {
         $("#downFileElement").click(function () {
-            location.href = basePath + "/api/files/downFile";
+            /**
+             * 使用location.href方式下载文件
+             * （1）谷歌浏览器：直接下载文件。
+             * （2）IE浏览器：直接下载文件。
+             * （3）火狐浏览器：打开保存对话框
+             *
+             */
+            //location.href = basePath + "/api/files/downFile";
+
+            /**
+             * 2018-10-24
+             * 使用window.open方式下载文件
+             * （1）谷歌浏览器：使用window.open会打开一个新窗口，快速关闭后下载文件。
+             * （2）IE浏览器：会打开一个下载文件对话框。
+             * （3）火狐浏览器：打开保存对话框
+             */
+            window.open(basePath + "/api/files/downFile");
+
         });
 
         $("#downFileElement1").click(function () {
