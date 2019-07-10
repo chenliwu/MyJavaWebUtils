45963ae7d978943689ba04b2a5a7399b74553bfd
BYTTER\chenlw <chenlw@bytter.com>
2019-07-10 19:33:16 +0800

    XML转化工具类

---
+++ src/main/java/com/charlie/ssm/demo/bank/icbc/ICBCMultipleAccQueryBalanceOrderInput.java
@@ -0,0 +1,190 @@
+package com.charlie.ssm.demo.bank.icbc;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+
+/**
+ * 如果客户提交单笔指令，则银行对该指令实时处理，返回指令级返回包。
+ * <p>
+ * QACCBAL指令级返回包
+ * 交易代码为：QACCBAL
+ * <p>
+ * 工商银行-多账号余额查询
+ * created by chenlw on 2019/07/10
+ */
+@Data
+@Accessors(chain = true)
+@JacksonXmlRootElement(localName = "CMS")
+public class ICBCMultipleAccQueryBalanceOrderInput {
+
+    private Eb eb;
+
+    @Data
+    public static class Eb {
+        private Pub pub;
+        private Out out;
+    }
+
+    @Data
+    public static class Pub {
+        /**
+         * 交易代码：QACCBAL
+         */
+        @JacksonXmlProperty(localName = "TransCode")
+        private String transCode;
+        /**
+         * 集团CIS号
+         */
+        @JacksonXmlProperty(localName = "CIS")
+        private String cis;
+        /**
+         * 归属银行编号
+         */
+        @JacksonXmlProperty(localName = "BankCode")
+        private String bankCode;
+        /**
+         * 证书ID
+         */
+        @JacksonXmlProperty(localName = "ID")
+        private String id;
+        /**
+         * 交易日期
+         */
+        @JacksonXmlProperty(localName = "TranDate")
+        private String tranDate;
+        /**
+         * 交易时间
+         */
+        @JacksonXmlProperty(localName = "TranTime")
+        private String tranTime;
+        /**
+         * 指令包序列号
+         */
+        @JacksonXmlProperty(localName = "fSeqno")
+        private String sequence;
+        /**
+         * 交易返回码
+         * 返回0为查询成功，具体返回信息显示在明细区
+         */
+        @JacksonXmlProperty(localName = "RetCode")
+        private String retCode;
+        /**
+         * 交易返回描述
+         */
+        @JacksonXmlProperty(localName = "RetMsg")
+        private String retMsg;
+    }
+
+    @Data
+    public static class Out {
+        /**
+         * 返回集合
+         */
+        @JacksonXmlElementWrapper(useWrapping = false)
+        private Collection<Rd> rd;
+    }
+
+    @Data
+    public static class Rd {
+        /**
+         * 指令顺序号
+         */
+        @JacksonXmlProperty(localName = "iSeqno")
+        private String iSeqno;
+
+        /**
+         * 账号
+         */
+        @JacksonXmlProperty(localName = "AccNo")
+        private String accNo;
+
+        /**
+         * 币种
+         */
+        @JacksonXmlProperty(localName = "CurrType")
+        private Long currType;
+
+        /**
+         * 钞汇标志
+         * 0-钞；1-汇；2-假汇；3-境外汇款
+         */
+        @JacksonXmlProperty(localName = "CashExf")
+        private Long cashExf;
+
+        /**
+         * 账户属性
+         * 第3位数字为帐户属性： 1：基本户 2：一般结算户 3：临时户 4：专用户 6：集团二级户 7：协定存款户 8：保证金户
+         * 查询帐户为定期户数据字典：0 国有企业1 外企及金融机构
+         */
+        @JacksonXmlProperty(localName = "AcctProperty")
+        private Long acctProperty;
+
+        /**
+         * 昨日余额
+         */
+        @JacksonXmlProperty(localName = "AccBalance")
+        private BigDecimal accBalance;
+
+        /**
+         * 当前余额
+         * 以币种的最小单位为单位
+         */
+        @JacksonXmlProperty(localName = "Balance")
+        private BigDecimal balance;
+
+        /**
+         * 可用余额
+         * 以币种的最小单位为单位
+         */
+        @JacksonXmlProperty(localName = "UsableBalance")
+        private BigDecimal usableBalance;
+
+        /**
+         * 冻结额度合计
+         * 以币种的最小单位为单位
+         */
+        @JacksonXmlProperty(localName = "FrzAmt")
+        private BigDecimal frzAmt;
+
+        /**
+         * 查询时间
+         * yyyyMMddHHmmssssssss
+         */
+        @JacksonXmlProperty(localName = "QueryTime")
+        private Long queryTime;
+
+
+        /**
+         * 明细交易返回码
+         */
+        @JacksonXmlProperty(localName = "iRetCode")
+        private String iRetCode;
+
+        /**
+         * 明细交易返回描述
+         */
+        @JacksonXmlProperty(localName = "iRetMsg")
+        private String iRetMsg;
+
+        /**
+         * 响应备用字段3
+         * 行别
+         */
+        @JacksonXmlProperty(localName = "RepReserved3")
+        private String repReserved3;
+
+        /**
+         * 响应备用字段4
+         * 启用为“信用额度”；以币种的最小金额为单位
+         */
+        @JacksonXmlProperty(localName = "RepReserved4")
+        private String repReserved4;
+    }
+
+}
--- src/main/java/com/charlie/ssm/demo/bank/icbc/ICBCMultipleAccQueryBalanceOutput.java
+++ src/main/java/com/charlie/ssm/demo/bank/icbc/ICBCMultipleAccQueryBalanceOutput.java
@@ -1,75 +1,29 @@
 package com.charlie.ssm.demo.bank.icbc;
 
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 /**
- * 工商银行-查询历史明细
+ * QACCBAL发送包
+ * 工商银行-多账号余额查询
+ * 交易代码为：QACCBAL
+ * <p>
  *
- * @author zuoy 2019-07-10
+ * 如果客户提交单笔指令，则银行对该指令实时处理，返回指令级返回包。
+ * 如果客户提交多笔指令，则银行返回文件级返回包，由银行后台系统异步处理指令；当客户开通主动反馈功能时，银行处理完毕后将指令级返回包主动发送给客户。
+ *
+ *
+ * created by chenlw on 2019/07/10
  */
 @Data
 @Accessors(chain = true)
 @JacksonXmlRootElement(localName = "CMS")
 public class ICBCMultipleAccQueryBalanceOutput {
-
-    public static void main(String[]args){
-        try {
-            test1();
-        }catch (Exception e){
-            System.out.println("error:"+e.getMessage());
-        }
-    }
-
-    public static void test1() throws Exception{
-        ICBCMultipleAccQueryBalanceOutput multipleAccQueryBalanceOutput = new ICBCMultipleAccQueryBalanceOutput();
-        Eb eb = new Eb();
-        Pub pub = new Pub();
-        pub.setTransCode("交易代码");
-        pub.setCis("集团CIS号");
-        pub.setBankCode("归属银行编号");
-        pub.setId("证书ID");
-        pub.setTranDate("交易日期");
-        pub.setTranTime("交易时间");
-        pub.setSequence("指令包序列号");
-
-        List<Rd> rdList = new ArrayList<>();
-        In in = new In();
-        in.setTotalNum(6L);
-        Rd rd = new Rd();
-        rd.setISeqno("指令顺序号");
-        rd.setAccNo("账号");
-        rd.setCurrType("RMB");
-        rdList.add(rd);
-        in.setRd(rdList);
-
-        eb.setPub(pub);
-        eb.setIn(in);
-        multipleAccQueryBalanceOutput.setEb(eb);
-
-        XmlMapper xmlMapper = new XmlMapper();
-        // 格式化
-       xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        // 设置xml头
-//        xmlMapper.enable(ToXmlGenerator.Feature.WRITE_XML_DECLARATION);
-        //字段为null，自动忽略，不再序列化
-//        xmlMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        //XML标签名:使用骆驼命名的属性名，
-//        xmlMapper.setPropertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE);
-        //设置转换模式
-//        xmlMapper.enable(MapperFeature.USE_STD_BEAN_NAMING);
-        String xml = xmlMapper.writeValueAsString(multipleAccQueryBalanceOutput);
-        System.out.println(xml);
-    }
 
     private Eb eb;
 
@@ -82,37 +36,43 @@ public class ICBCMultipleAccQueryBalanceOutput {
     @Data
     public static class Pub {
         /**
-         * 交易代码
+         * 交易代码：QACCBAL
          */
         @JacksonXmlProperty(localName = "TransCode")
         private String transCode;
         /**
          * 集团CIS号
+         * 客户注册时的归属编码
          */
         @JacksonXmlProperty(localName = "CIS")
         private String cis;
         /**
          * 归属银行编号
+         * 客户注册时的归属单位
          */
         @JacksonXmlProperty(localName = "BankCode")
         private String bankCode;
         /**
          * 证书ID
+         * 无证书客户可上送空
          */
         @JacksonXmlProperty(localName = "ID")
         private String id;
         /**
          * 交易日期
+         * ERP系统产生的交易日期，格式是yyyyMMdd
          */
         @JacksonXmlProperty(localName = "TranDate")
         private String tranDate;
         /**
          * 交易时间
+         * ERP系统产生的交易时间，格式如HHmmssSSS，精确到毫秒
          */
         @JacksonXmlProperty(localName = "TranTime")
         private String tranTime;
         /**
          * 指令包序列号
+         * ERP系统产生的指令包序列号，一个集团永远不能重复
          */
         @JacksonXmlProperty(localName = "fSeqno")
         private String sequence;
@@ -123,23 +83,26 @@ public class ICBCMultipleAccQueryBalanceOutput {
 
         /**
          * 总笔数
+         * 需要查询的账号的个数，即提交包明细的笔数。
          */
         @JacksonXmlProperty(localName = "TotalNum")
         private Long totalNum;
 
         /**
          * 响应备用字段1
+         * 备用，目前无意义
          */
         @JacksonXmlProperty(localName = "RepReserved1")
         private String repReserved1;
 
         /**
          * 响应备用字段2
+         * 备用，目前无意义
          */
         @JacksonXmlProperty(localName = "ReqReserved2")
         private String repReserved2;
 
-        @JacksonXmlElementWrapper(localName = "rd",useWrapping = false)
+        @JacksonXmlElementWrapper(localName = "rd", useWrapping = false)
         private Collection<Rd> rd;
 
     }
@@ -149,6 +112,7 @@ public class ICBCMultipleAccQueryBalanceOutput {
 
         /**
          * 指令顺序号
+         * 不能为空，并且不能重复
          */
         @JacksonXmlProperty(localName = "iSeqno")
         private String iSeqno;
@@ -161,6 +125,7 @@ public class ICBCMultipleAccQueryBalanceOutput {
 
         /**
          * 币种
+         * 工行币种代码,不输入则自动取账号币种
          */
         @JacksonXmlProperty(localName = "CurrType")
         private String currType;
@@ -168,12 +133,14 @@ public class ICBCMultipleAccQueryBalanceOutput {
 
         /**
          * 响应备用字段3
+         * 行别,集团有他行账号时为必输项（输入102或不输入为工行，其他为他行）
          */
         @JacksonXmlProperty(localName = "RepReserved3")
         private String repReserved3;
 
         /**
          * 响应备用字段4
+         * 备用，目前无意义
          */
         @JacksonXmlProperty(localName = "RepReserved4")
         private String repReserved4;
