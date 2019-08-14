496a6fe6f3f84ddcc218423aa4a3d21ce6cd46c5
BYTTER\chenlw <chenlw@bytter.com>
2019-08-14 10:29:55 +0800

    截取GBK字节数组

--- src/main/java/com/chenlw/java/web/utils/java/learning/string/StringTest.java
+++ src/main/java/com/chenlw/java/web/utils/java/learning/string/StringTest.java
@@ -8,8 +8,10 @@ public class StringTest {
 
     public static void main(String[] args) {
         try {
-            //testByteArray();
-            testStringSplit();
+            // testByteArray();
+            // testStringSplit();
+            testByteArray();
+            testBgkByteArrTransfer();
         } catch (Exception e) {
             System.out.println("异常信息：" + e.getMessage());
         }
@@ -24,20 +26,50 @@ public class StringTest {
     }
 
 
+    /**
+     * 解析String定长串
+     * 截取GBK字节数组
+     *
+     * @throws Exception
+     */
     public static void testBgkByteArrTransfer() throws Exception {
         // 定长串  60 30 18 30 = 138
         String srcStr1 = "客户姓名0                                                   客户账号0                     1000.00           摘要0                         ";
         byte[] gbkByteArr = srcStr1.getBytes("GBK");
+        byte[] accountNameByteArr = subByte(gbkByteArr, 0, 60);
+        String accountName = new String(accountNameByteArr, "GBK");
+        System.out.println(accountName + "|");
+        System.out.println(accountName.trim() + "|");
 
+        byte[] accountByteArr = subByte(gbkByteArr, 60, 30);
+        String account = new String(accountByteArr, "GBK");
+        System.out.println(account + "|");
+        System.out.println(account.trim() + "|");
+
+        byte[] amountByteArr = subByte(gbkByteArr, 90, 18);
+        String amount = new String(amountByteArr, "GBK");
+        System.out.println(amount + "|");
+        System.out.println(amount.trim() + "|");
+
+        byte[] summaryByteArr = subByte(gbkByteArr, 108, 30);
+        String summary = new String(summaryByteArr, "GBK");
+        System.out.println(summary + "|");
+        System.out.println(summary.trim() + "|");
     }
 
-    public static void testStringSplit(){
-        String srcStr1 = "客户姓名0                                                   客户账号0                     1000.00           摘要0                         ";
-        String[] strArr = srcStr1.split(" ");
-        System.out.println("拆分数组长度："+strArr.length);
-        for(String str:strArr){
-            System.out.println(str);
-        }
+
+    /**
+     * 截取byte数组   不改变原数组
+     *
+     * @param b      原数组
+     * @param off    偏差值（索引）
+     * @param length 长度
+     * @return 截取后的数组
+     */
+    public static byte[] subByte(byte[] b, int off, int length) {
+        byte[] b1 = new byte[length];
+        System.arraycopy(b, off, b1, 0, length);
+        return b1;
     }
 
 
