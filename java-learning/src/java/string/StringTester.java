ed53fe8200b4e32018bc4df9b9585598e2cd4496
BYTTER\chenlw <chenlw@bytter.com>
2019-09-05 13:47:22 +0800

    字符串解析

--- java-learning/src/java/string/StringTester.java
+++ java-learning/src/java/string/StringTester.java
@@ -9,9 +9,34 @@ public class StringTester {
 
 
     public static void main(String[]args){
-        testResolveString();
+        // testResolveString();
+
+        //String datetimeString = "2019090511220605";
+        String datetimeString = "201909051122";
+        System.out.println(getDateString(datetimeString));
     }
 
+    /**
+     * 获取yyyyMMddHHmmss日期时间字符串
+     *
+     * @param datetimeString
+     * @return
+     */
+    public static String getDateString(String datetimeString) {
+        if (datetimeString.length() == 14) {
+            return datetimeString;
+        }
+        StringBuilder sb = new StringBuilder();
+        if (datetimeString.length() > 14) {
+            sb.append(datetimeString.substring(0, 14));
+        } else {
+            sb.append(datetimeString);
+            for (int i = 0, len = 14 - datetimeString.length(); i < len; i++) {
+                sb.append("0");
+            }
+        }
+        return sb.toString();
+    }
 
     public static void testResolveString(){
         String transStatusListString = "0:提交成功,等待银行处理|1:授权成功, 等待银行处理|2:等待授权|3:等待二次授权|4:等待银行答复|5:主机返回待处理|6:被银行拒绝|7:处理成功|8:指令被拒绝授权|9:银行正在处理|10:预约指令|11:预约取消|86:等待电话核实|95:待核查|98:区域中心通讯可疑|";
