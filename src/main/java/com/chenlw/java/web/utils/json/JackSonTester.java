75e6b8bbea881229459e1bc1ea20276ed0b5e10b
chenlw <30075213@qq.com>
2020-07-08 23:25:09 +0800

    update

---
+++ src/main/java/com/chenlw/java/web/utils/json/JackSonTester.java
@@ -0,0 +1,20 @@
+package com.chenlw.java.web.utils.json;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+/**
+ * @author chenlw
+ * @since 2020-06-01
+ */
+public class JackSonTester {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void test(){
+        ArrayNode arrayNode;
+    }
+
+
+}
